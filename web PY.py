a696c53e00101048e3c4443144a45159ff552cdd
xiaoQsang <a574870150@gmail.com>
2018-07-02 17:32:09 +0800

    'web'

--- web PY.py
+++ web PY.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from countdays import count_days_between_two_years
 app = Flask(__name__)
 
 
@@ -6,8 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/aboutMe', methods=["GET", "POST"])
-def about_Me():
-    return "My name is Wolfgang.Ji"
+@app.route('/countdays', methods=["GET", "POST"])
+def countdays():
+    if request.method == 'POST':
+        start = request.form['start']
+        end = request.form['end']
+        total = count_days_between_two_years(start, end)
+        return render_template('countdays.html', countDays= total, hidden="")
+    else:
+        return render_template('countdays.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
