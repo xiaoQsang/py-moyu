a62524878620cd95d10e83672c6dddc5c8feeab9
xiaoQsang <a574870150@gmail.com>
2018-07-04 10:31:04 +0800

    'web'

--- countdays.py
+++ countdays.py
@@ -1,88 +1,94 @@
-def is_R_or_N(user_in_date=2017):
+# coding=utf-8
+def is_R_or_N(user_in_date=2017):  # 判断是否闰年
     value_r_n = (user_in_date % 4 == 0 and user_in_date % 100 != 0) \
-            or user_in_date % 400 ==0
+                or user_in_date % 400 == 0
     return value_r_n
 
-def get_mend_days(mEnd=12, is_R_N=True):
+
+def getBeforeMonthsTotalDays(mEnd=12, is_R_N=True):
     totaldays = 0
     if is_R_N:
         if mEnd == 12:
-            totaldays= 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
+            totaldays = 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 11:
-            totaldays= 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
+            totaldays = 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 10:
-            totaldays= 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
+            totaldays = 30 + 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 9:
-            totaldays= 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
+            totaldays = 31 + 31 + 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 8:
-            totaldays= 31 + 30 + 31 +  30 + 31 + 29 + 31
+            totaldays = 31 + 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 7:
-            totaldays= 30 + 31 + 30 + 31 + 29 + 31
+            totaldays = 30 + 31 + 30 + 31 + 29 + 31
         elif mEnd == 6:
-            totaldays= 31 + 30 + 31 + 29 + 31
+            totaldays = 31 + 30 + 31 + 29 + 31
         elif mEnd == 5:
-            totaldays= 30 + 31 + 29 + 31
+            totaldays = 30 + 31 + 29 + 31
         elif mEnd == 4:
-            totaldays= 31 + 29 + 31
+            totaldays = 31 + 29 + 31
         elif mEnd == 3:
-            totaldays= 29 + 31
+            totaldays = 29 + 31
         elif mEnd == 2:
-            totaldays= 31
+            totaldays = 31
         elif mEnd == 1:
-            totaldays= 0
+            totaldays = 0
     else:
         if mEnd == 12:
-            totaldays= 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
+            totaldays = 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 11:
-            totaldays= 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
+            totaldays = 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 10:
-            totaldays= 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
+            totaldays = 30 + 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 9:
-            totaldays= 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
+            totaldays = 31 + 31 + 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 8:
-            totaldays= 31 + 30 + 31 +  30 + 31 + 28 + 31
+            totaldays = 31 + 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 7:
-            totaldays= 30 + 31 + 30 + 31 + 28 + 31
+            totaldays = 30 + 31 + 30 + 31 + 28 + 31
         elif mEnd == 6:
-            totaldays= 31 + 30 + 31 + 28 + 31
+            totaldays = 31 + 30 + 31 + 28 + 31
         elif mEnd == 5:
-            totaldays= 30 + 31 + 28 + 31
+            totaldays = 30 + 31 + 28 + 31
         elif mEnd == 4:
-            totaldays= 31 + 28 + 31
+            totaldays = 31 + 28 + 31
         elif mEnd == 3:
-            totaldays= 28 + 31
+            totaldays = 28 + 31
         elif mEnd == 2:
-            totaldays= 31
+            totaldays = 31
         elif mEnd == 1:
-            totaldays= 0
+            totaldays = 0
     return totaldays
-def get_total_days_of_one_year(year=2017, is_R_N=True):
-    if is_R_N :
-        one_year = 366
-    else:
-        one_year = 365
-    return one_year
 
-def get_before_years_total_days(yEnd=2017, yStart=2000):
-    totaldays = 0
-    years = range(yStart, yEnd)
+
+def getTotalDaysOneWholeYear(year=2017):
+    if is_R_or_N(year):
+        total = 366
+    else:
+        total = 365
+    return total
+
+
+def getBeforeYearsTotalDays(yEnd=2017):
+    totalDays = 0
+    years = range(1, yEnd)
     for year_every in years:
         if is_R_or_N(year_every):
-            totaldays += 366
-            totaldays = totaldays + 366
+            totalDays += 366
         else:
-            totaldays = totaldays + 365
-    return totaldays
-def count_days_between_two_years(Sday="20000701",Eday="20180701"):
-    yStart, mStart, dStart = int(Sday[:4]), int(Sday[4:6]), int(Sday[6:8])
-    yEnd, mEnd, dEnd, = int(Eday[:4]), int(Eday[4:6]), int(Eday[6:8])
+            totalDays += 365
+    return totalDays
+
+
+def countDaysDiffer(dayStart='20150101', dayEnd='20170102'):
+    yStart, mStart, dStart = int(dayStart[:4]), int(dayStart[4:6]), int(dayStart[6:8])
+    yEnd, mEnd, dEnd = int(dayEnd[:4]), int(dayEnd[4:6]), int(dayEnd[6:8])
     print('dayStart:', yStart, mStart, dStart)
     print('dayEnd:', yEnd, mEnd, dEnd)
-    EndDays = get_before_years_total_days(yEnd) + get_mend_days(mEnd,is_R_or_N(yEnd)) + dEnd
-    StartDays = get_before_years_total_days(yStart) + get_mend_days(mStart, is_R_or_N(yStart)) + dStart
+    EndDays = getBeforeYearsTotalDays(yEnd) + getBeforeMonthsTotalDays(mEnd, is_R_or_N(yEnd)) + dEnd
+    StartDays = getBeforeYearsTotalDays(yStart) + getBeforeMonthsTotalDays(mStart, is_R_or_N(yStart)) + dStart
     return EndDays - StartDays
 
 if __name__ == '__main__':
 
-    days = count_days_between_two_years('20010820','20180702')
-    print('days:', days)
+    days = countDaysDiffer('20040731', '20180701')
+    print('days：', days)
---
+++ webPY.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+from countdays import countDaysDiffer
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        start = request.form['start']
+        end = request.form['end']
+        total = countDaysDiffer(start, end)
+        return render_template('index.html', countDays=total, hidden='')
+    else:
+        return render_template('index.html', hidden='hidden')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
